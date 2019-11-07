be1383bb5260fd12fee6e8f74298114d7bd23caf
Khoa Do <khoado.dk@gmail.com>
2019-11-07 12:10:32 -0500

    added user routes and functionality

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const expressValidator = require('express-validator');
+require('dotenv').config();
+
+const app = express();
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    console.log('MongoDB connected!');
+  });
+
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(expressValidator());
+
+const userRoutes = require('./routes/user');
+app.use('/api', userRoutes);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
