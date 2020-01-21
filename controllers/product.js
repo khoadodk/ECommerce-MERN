3eb10fc0d1316143d51877398fb24a8975798401
Khoa Do <khoado.dk@gmail.com>
2020-01-21 11:54:18 -0500

    Added CRUD methods to product

--- controllers/product.js
+++ controllers/product.js
@@ -5,6 +5,72 @@ const fs = require('fs');
 const Product = require('../models/product');
 const { errorHandler } = require('../helpers/dbErrorHandler');
 
+exports.productById = (req, res, next, id) => {
+  Product.findById(id).exec((err, product) => {
+    if (err || !product)
+      return res.status(400).json({ error: 'The product does not exist' });
+    req.product = product;
+    next();
+  });
+};
+
+exports.read = (req, res) => {
+  req.product.photo = undefined;
+  return res.json(req.product);
+};
+
+exports.remove = (req, res) => {
+  let product = req.product;
+  product.remove((err, deletedProduct) => {
+    if (err || !product)
+      return res.status(400).json({ error: 'The product does not exist' });
+    res.json({
+      message: 'Product deleted successfully'
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  //Parses an incoming node.js request containing form data
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: 'Image could not be uploaded'
+      });
+    }
+
+    let product = req.product;
+    //using Lodash to deep clone the object
+    product = _.extend(product, fields);
+
+    // 1kb = 1000
+    // 1mb = 1000000
+
+    if (files.photo) {
+      console.log('FILES PHOTO: ', files.photo);
+      if (files.photo.size > 1000000) {
+        return res.status(400).json({
+          error: 'Image should be less than 1 MB in size'
+        });
+      }
+      //read the photo file and save into database
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err)
+        });
+      }
+      res.json(result);
+    });
+  });
+};
+
 exports.create = (req, res) => {
   let form = new formidable.IncomingForm();
   form.keepExtensions = true;
--- routes/product.js
+++ routes/product.js
@@ -1,15 +1,38 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/product');
+const {
+  read,
+  create,
+  productById,
+  update,
+  remove
+} = require('../controllers/product');
 
 //middleware routes
 const { userById } = require('../controllers/user');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
+// CRUD Methods
+router.get('/product/:productId', read);
+router.put(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+router.delete(
+  '/product/:productId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
 router.post('/product/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 //run the middleware finduserById when there is a param of :userId
 router.param('userId', userById);
+router.param('productId', productById);
 
 module.exports = router;
