656294fb5085cfb3ec6d1433a0ff52dba9d19411
Khoa Do <khoado.dk@gmail.com>
2020-01-21 12:26:40 -0500

    Added CRUD methods to category

--- controllers/category.js
+++ controllers/category.js
@@ -1,5 +1,46 @@
 const Category = require('../models/category');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const _ = require('lodash');
+
+exports.categoryById = (req, res, next, id) => {
+  Category.findById(id).exec((err, category) => {
+    if (err || !category)
+      return res.status(400).json({ error: 'The category does not exist' });
+    req.category = category;
+    next();
+  });
+};
+
+exports.getAll = (req, res) => {
+  Category.find().exec((err, data) => {
+    if (err) return res.status(400).json({ error: errorHandler(err) });
+    res.json(data);
+  });
+};
+
+exports.read = (req, res) => {
+  return res.json(req.category);
+};
+
+exports.remove = (req, res) => {
+  let category = req.category;
+  category.remove((err, deletedCategory) => {
+    if (err || !category)
+      return res.status(400).json({ error: 'The category does not exist' });
+    res.json({
+      message: 'Category deleted successfully'
+    });
+  });
+};
+
+exports.update = (req, res) => {
+  const updatedFields = req.body;
+  const updatedCategory = _.extend(req.category, updatedFields);
+  updatedCategory.save((err, data) => {
+    if (err) return res.status(400).json({ error: errorHandler(err) });
+    res.json({ data });
+  });
+};
 
 exports.create = (req, res) => {
   const category = new Category(req.body);
--- routes/category.js
+++ routes/category.js
@@ -1,15 +1,40 @@
 const express = require('express');
 const router = express.Router();
 
-const { create } = require('../controllers/category');
+const {
+  getAll,
+  read,
+  remove,
+  update,
+  create,
+  categoryById
+} = require('../controllers/category');
 
 //middleware routes
 const { userById } = require('../controllers/user');
 const { requireSignin, isAdmin, isAuth } = require('../controllers/auth');
 
+//CRUD methods
+router.get('/categories', getAll);
+router.get('/category/:categoryId', read);
+router.put(
+  '/category/:categoryId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  update
+);
+router.delete(
+  '/category/:categoryId/:userId',
+  requireSignin,
+  isAuth,
+  isAdmin,
+  remove
+);
 router.post('/category/create/:userId', requireSignin, isAuth, isAdmin, create);
 
 //run the middleware finduserById when there is a param of :userId
 router.param('userId', userById);
+router.param('categoryId', categoryById);
 
 module.exports = router;
