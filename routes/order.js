2f882d13444ca776324001e81167fc9c3d50df1f
Khoa Do <khoado.dk@gmail.com>
2020-01-25 21:57:44 -0500

    Added middleware route to add order to user's history

--- routes/order.js
+++ routes/order.js
@@ -2,10 +2,16 @@ const express = require('express');
 const router = express.Router();
 
 const { requireSignin, isAuth } = require('../controllers/auth');
-const { userById } = require('../controllers/user');
+const { userById, addOrderToUserHistory } = require('../controllers/user');
 const { create } = require('../controllers/order');
 
-router.post('/order/create/:userId', requireSignin, isAuth, create);
+router.post(
+  '/order/create/:userId',
+  requireSignin,
+  isAuth,
+  addOrderToUserHistory,
+  create
+);
 
 router.param('userId', userById);
 module.exports = router;
