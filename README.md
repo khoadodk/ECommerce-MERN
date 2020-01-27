adf9d300d4ed2dd6ccba3dacd87e1d0d9fe0a4fc
Khoa Do <khoado.dk@gmail.com>
2020-01-27 16:29:38 -0500

    setting up heroku for deployment

---
+++ README.md
@@ -0,0 +1,17 @@
+# E-Commerce Store with MERN Stack
+
+This app is the follow up of my boilerplate-MERN:
+https://github.com/khoadodk/boilerplate-mern-stack
+
+# App features
+
+<ul>
+<li>Login/Signup system</li>
+<li>User/Admin dashBoard</li>
+<li>Products filtered by category or price</li>
+<li>Product Search</li>
+<li>Product page with related products</li>
+<li>Shopping cart</li>
+<li>Payment with Credit Card or PayPal</li>
+<li>Email notification when an order is placed</li>
+</ul>
--- index.js
+++ index.js
@@ -41,6 +41,15 @@ app.use('/api', productRoutes);
 app.use('/api', braintreeRoutes);
 app.use('/api', orderRoutes);
 
+//heroku production environment setup
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
