990f40e440a2f1eefcfedbdbb13eb00ffbeb6685
Khoa Do <khoado.dk@gmail.com>
2020-01-27 14:01:31 -0500

    Added sendgrid order notification to admin email

--- controllers/order.js
+++ controllers/order.js
@@ -1,5 +1,7 @@
-const { Order, CartItem } = require('../models/order');
+const { Order } = require('../models/order');
 const { errorHandler } = require('../helpers/dbErrorHandler');
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 exports.orderById = (req, res, next, id) => {
   Order.findById(id)
@@ -13,19 +15,6 @@ exports.orderById = (req, res, next, id) => {
       req.order = order;
       next();
     });
-};
-
-exports.create = (req, res) => {
-  req.body.order.user = req.profile;
-  const order = new Order(req.body.order);
-  order.save((error, data) => {
-    if (error) {
-      return res.status(400).json({
-        error: errorHandler(error)
-      });
-    }
-    res.json(data);
-  });
 };
 
 exports.getOrders = (req, res) => {
@@ -59,4 +48,89 @@ exports.updateOrderStatus = (req, res) => {
       res.json(order);
     }
   );
+};
+
+exports.create = (req, res) => {
+  console.log('CREATE ORDER: ', req.body);
+  req.body.order.user = req.profile;
+  const order = new Order(req.body.order);
+  order.save((error, data) => {
+    if (error) {
+      return res.status(400).json({
+        error: errorHandler(error)
+      });
+    }
+    // User.find({ categories: { $in: categories } }).exec((err, users) => {}
+    console.log('ORDER IS JUST SAVED >>> ', order);
+    // send email alert to admin
+    // order.address
+    // order.products.length
+    // order.amount
+    const emailData = {
+      to: 'khoado.dk@gmail.com', // admin
+      from: 'noreply@ecommerce.com',
+      subject: `A new order is received`,
+      html: `
+            <h1>Hey Admin, somebody just made a purchase in your ecommerce store</h1>
+            <h2>Customer name: ${order.user.name}</h2>
+            <h2>Customer address: ${order.address}</h2>
+            <h2>User's purchase history: ${
+              order.user.history.length
+            } purchase</h2>
+            <h2>User's email: ${order.user.email}</h2>
+            <h2>Total products: ${order.products.length}</h2>
+            <h2>Transaction ID: ${order.transaction_id}</h2>
+            <h2>Order status: ${order.status}</h2>
+            <h2>Product details:</h2>
+            <hr />
+            ${order.products
+              .map(p => {
+                return `<div>
+                        <h3>Product Name: ${p.name}</h3>
+                        <h3>Product Price: ${p.price}</h3>
+                        <h3>Product Quantity: ${p.count}</h3>
+                </div>`;
+              })
+              .join('--------------------')}
+            <h2>Total order cost: ${order.amount}<h2>
+            <p>Login to your dashboard</a> to see the order in detail.</p>
+        `
+    };
+    sgMail
+      .send(emailData)
+      .then(sent => console.log('SENT >>>', sent))
+      .catch(err => console.log('ERR >>>', err));
+
+    // email to buyer
+    const emailData2 = {
+      to: order.user.email,
+      from: 'noreply@ecommerce.com',
+      subject: `You order is in process`,
+      html: `
+            <h1>Hey ${req.profile.name}, Thank you for shopping with us.</h1>
+            <h2>Total products: ${order.products.length}</h2>
+            <h2>Transaction ID: ${order.transaction_id}</h2>
+            <h2>Order status: ${order.status}</h2>
+            <h2>Product details:</h2>
+            <hr />
+            ${order.products
+              .map(p => {
+                return `<div>
+                        <h3>Product Name: ${p.name}</h3>
+                        <h3>Product Price: ${p.price}</h3>
+                        <h3>Product Quantity: ${p.count}</h3>
+                </div>`;
+              })
+              .join('--------------------')}
+            <h2>Total order cost: ${order.amount}<h2>
+            <p>Thank you for shopping with us.</p>
+        `
+    };
+    sgMail
+      .send(emailData2)
+      .then(sent => console.log('SENT 2 >>>', sent))
+      .catch(err => console.log('ERR 2 >>>', err));
+
+    res.json(data);
+  });
 };
