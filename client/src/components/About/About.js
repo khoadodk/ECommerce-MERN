3181d838069964a626fdda760c124fb02b7bc6cb
Khoa Do <khoado.dk@gmail.com>
2020-01-31 20:33:28 -0500

    Added a footer to about page

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
@@ -26,8 +27,34 @@ const Home = () => {
           <li>Payment with PayPal</li>
         </ul>
       </div>
-      <footer className="text-center">Created by &copy; Khoa Do 2020</footer>
+
+      <footer className="bg-dark p-2 fixed-bottom">
+        <div className="container-fluid text-white">
+          <div className="row">
+            <div className="col-6">
+              <p className="mb-0 ">
+                Copyright &copy; 2020 All Rights Reserved by Khoa Do
+              </p>
+            </div>
+            <div className="col-6 d-flex justify-content-center align-items-center">
+              <div className="mr-3">
+                <i className="fab fa-facebook "></i>
+              </div>
+              <div className="mr-3">
+                <i className="fab fa-linkedin"></i>
+              </div>
+              <div className="mr-3">
+                <i className="fab fa-dribbble"></i>
+              </div>
+              <div className="mr-3">
+                <i className="fab fa-instagram"></i>
+              </div>
+            </div>
+          </div>
+        </div>
+      </footer>
     </div>
   );
 };
+
 export default Home;
